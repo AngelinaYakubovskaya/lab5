6d01137bf6000bd63427f7ded1438bb3336784ae
AngelinaYakubovskaya <146866578+AngelinaYakubovskaya@users.noreply.github.com>
2024-09-27 10:44:13 +0300

    Delete Lab5.cpp

--- Lab5.cpp
+++
@@ -1,9 +0,0 @@
-﻿#include "Server.h"
-
-int main() {
-    // Создание и запуск сервера на порту 8080
-    Server server(8080);
-    std::cout << "Waiting for client connections..." << std::endl;
-    server.start();
-    return 0;
-}
