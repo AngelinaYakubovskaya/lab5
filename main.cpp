4a21d29208f0610f5dd08b9ad69f8003b144a47b
AngelinaYakubovskaya <146866578+AngelinaYakubovskaya@users.noreply.github.com>
2024-09-27 10:44:53 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,9 @@
+﻿#include "Server.h"
+
+int main() {
+    // Создание и запуск сервера на порту 8080
+    Server server(8080);
+    std::cout << "Waiting for client connections..." << std::endl;
+    server.start();
+    return 0;
+}
